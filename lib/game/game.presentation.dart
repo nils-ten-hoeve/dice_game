70c6dd31bfdd3c5c2478e882a221a271a2a19c4d
Nils ten Hoeve <nilstenhoeve@gmail.com>
2025-01-01 17:53:41 +0100

    Fixed #3

--- lib/game/game.presentation.dart
+++ lib/game/game.presentation.dart
@@ -15,49 +15,47 @@ class _GameWidgetState extends State<GameWidget> {
   final GameService gameService = GameService();
 
   @override
-  Widget build(BuildContext context) => ListenableBuilder(
-        listenable: gameService,
-        builder: (context, child) {
-          var game = gameService.currentGame;
-          return Expanded(
-            child: Align(
-              alignment: AlignmentDirectional.center,
-              child: AspectRatio(
-                aspectRatio: 5 / 3,
-                child: ListenableBuilder(
-                  listenable: game,
-                  builder: (context, child) {
-                    if (game.finished) {
-                      WidgetsBinding.instance.addPostFrameCallback((_) {
-                        showFinishedDialog(context);
-                      });
-                    }
-                    if (game.dutchMessage != null) {
-                      WidgetsBinding.instance.addPostFrameCallback((_) {
-                        showMessageDialog(context);
-                      });
-                    }
-                    return Column(
-                      children: [
-                        GameRow(game: game, cellRow: game.variant.row1),
-                        Spacer(),
-                        GameRow(game: game, cellRow: game.variant.row2),
-                        Spacer(),
-                        GameRow(game: game, cellRow: game.variant.row3),
-                        Spacer(),
-                        GameRow(game: game, cellRow: game.variant.row4),
-                        Spacer(),
-                        ScoreAndActionBar(),
-                        Spacer(),
-                        if (game.showDice) DiceBar(),
-                        if (game.showDice) Spacer(),
-                      ],
-                    );
-                  },
-                ),
-              ),
-            ),
-          );
-        },
+  Widget build(BuildContext context) => Align(
+        alignment: Alignment.center,
+        child: AspectRatio(
+          aspectRatio: 5 / 3,
+          child: ListenableBuilder(
+            listenable: gameService,
+            builder: (context, child) {
+              var game = gameService.currentGame;
+              return ListenableBuilder(
+                listenable: game,
+                builder: (context, child) {
+                  if (game.finished) {
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+                      showFinishedDialog(context);
+                    });
+                  }
+                  if (game.dutchMessage != null) {
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+                      showMessageDialog(context);
+                    });
+                  }
+                  return Column(
+                    children: [
+                      GameRow(game: game, cellRow: game.variant.row1),
+                      Spacer(),
+                      GameRow(game: game, cellRow: game.variant.row2),
+                      Spacer(),
+                      GameRow(game: game, cellRow: game.variant.row3),
+                      Spacer(),
+                      GameRow(game: game, cellRow: game.variant.row4),
+                      Spacer(),
+                      ScoreAndActionBar(),
+                      Spacer(),
+                      if (game.showDice) DiceBar(),
+                      if (game.showDice) Spacer(),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ),
       );
 }
