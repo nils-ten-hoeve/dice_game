38f417b518a4287873a44e1ee66356687a496899
Nils ten Hoeve <nilstenhoeve@gmail.com>
2025-01-04 14:46:54 +0100

    Fixed #7

--- lib/game/game.presentation.dart
+++ lib/game/game.presentation.dart
@@ -3,6 +3,7 @@ import 'package:dice_game/game/cell/cell.presentation.dart';
 import 'package:dice_game/game/dice/dice.presentation.dart';
 import 'package:dice_game/game/game.service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class GameWidget extends StatefulWidget {
   const GameWidget({super.key});
@@ -15,47 +16,53 @@ class _GameWidgetState extends State<GameWidget> {
   final GameService gameService = GameService();
 
   @override
-  Widget build(BuildContext context) => Align(
-        alignment: Alignment.center,
-        child: AspectRatio(
-          aspectRatio: 15 / 8,
-          child: ListenableBuilder(
-            listenable: gameService,
-            builder: (context, child) {
-              var game = gameService.currentGame;
-              return ListenableBuilder(
-                listenable: game,
-                builder: (context, child) {
-                  if (game.finished) {
-                    WidgetsBinding.instance.addPostFrameCallback((_) {
-                      showFinishedDialog(context);
-                    });
-                  }
-                  if (game.dutchMessage != null) {
-                    WidgetsBinding.instance.addPostFrameCallback((_) {
-                      showMessageDialog(context);
-                    });
-                  }
-                  return Column(
-                    children: [
-                      GameRow(game: game, cellRow: game.variant.row1),
-                      Spacer(),
-                      GameRow(game: game, cellRow: game.variant.row2),
-                      Spacer(),
-                      GameRow(game: game, cellRow: game.variant.row3),
-                      Spacer(),
-                      GameRow(game: game, cellRow: game.variant.row4),
-                      Spacer(),
-                      ScoreAndActionBar(),
-                      Spacer(),
-                      if (game.showDice) DiceBar(),
-                      if (game.showDice) Spacer(),
-                    ],
-                  );
-                },
-              );
-            },
-          ),
+  Widget build(BuildContext context) {
+    hideSystemBar();
+    return Align(
+      alignment: Alignment.center,
+      child: AspectRatio(
+        aspectRatio: 15 / 8,
+        child: ListenableBuilder(
+          listenable: gameService,
+          builder: (context, child) {
+            var game = gameService.currentGame;
+            return ListenableBuilder(
+              listenable: game,
+              builder: (context, child) {
+                if (game.finished) {
+                  WidgetsBinding.instance.addPostFrameCallback((_) {
+                    showFinishedDialog(context);
+                  });
+                }
+                if (game.dutchMessage != null) {
+                  WidgetsBinding.instance.addPostFrameCallback((_) {
+                    showMessageDialog(context);
+                  });
+                }
+                return Column(
+                  children: [
+                    GameRow(game: game, cellRow: game.variant.row1),
+                    Spacer(),
+                    GameRow(game: game, cellRow: game.variant.row2),
+                    Spacer(),
+                    GameRow(game: game, cellRow: game.variant.row3),
+                    Spacer(),
+                    GameRow(game: game, cellRow: game.variant.row4),
+                    Spacer(),
+                    ScoreAndActionBar(),
+                    Spacer(),
+                    if (game.showDice) DiceBar(),
+                  ],
+                );
+              },
+            );
+          },
         ),
-      );
+      ),
+    );
+  }
+
+  void hideSystemBar() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+  }
 }
